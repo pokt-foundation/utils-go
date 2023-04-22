3ef2013bd32e0ba94d912ded70722cfb8b6d2c13
Roniel Valdez <roniel_valdez@outlook.com>
2023-04-22 01:34:13 -0400

    feat: extract domain from url

---
+++ domain/domain.go
@@ -0,0 +1,35 @@
+package domain
+
+import (
+	"errors"
+	"net"
+	"net/url"
+	"strings"
+)
+
+var (
+	errInvalidURL = errors.New("invalid URL")
+)
+
+func extractDomain(inputURL string) (string, error) {
+	parsedURL, err := url.Parse(inputURL)
+	if err != nil {
+		return "", errInvalidURL
+	}
+	if parsedURL.Hostname() == "" {
+		return "", errInvalidURL
+	}
+	// Split the hostname into parts separated by dots
+	parts := strings.Split(parsedURL.Hostname(), ".")
+	if len(parts) > 2 && !isIP(parts[len(parts)-2]) {
+		// If the last two parts of the hostname are not an IP address, join the last two parts
+		return strings.Join(parts[len(parts)-2:], "."), nil
+	}
+	// Otherwise, return the full hostname
+	return parsedURL.Hostname(), nil
+}
+
+// isIP checks whether the given string is a valid IP address
+func isIP(str string) bool {
+	return net.ParseIP(str) != nil
+}
---
+++ domain/domain_test.go
@@ -0,0 +1,43 @@
+package domain
+
+import (
+	"testing"
+)
+
+func TestExtractDomain(t *testing.T) {
+	// Valid URLs
+	validTests := []struct {
+		inputURL       string
+		expectedDomain string
+		expectedErr    error
+	}{
+		{"https://www.google.com", "google.com", nil},
+		{"https://github.com/openai", "github.com", nil},
+		{"http://localhost:8080", "localhost", nil},
+		{"https://node123.node.com:443", "node.com", nil},
+		{"https://node92.whiteholestake.io:443", "whiteholestake.io", nil},
+		{"https://node123.nodegroup.node.com:443", "node.com", nil},
+		{"https://node123.node-provider.com:443", "node-provider.com", nil},
+		{"not a valid url", "", errInvalidURL},
+		{"http://", "", errInvalidURL},
+		{"", "", errInvalidURL},
+	}
+	for _, tc := range validTests {
+		domain, err := extractDomain(tc.inputURL)
+		if err != tc.expectedErr {
+			t.Errorf("expected error: %v, got: %v", tc.expectedErr, err)
+		}
+		if domain != tc.expectedDomain {
+			t.Errorf("extractDomain(%q) = %q, expected %q", tc.inputURL, domain, tc.expectedDomain)
+		}
+	}
+
+	// Invalid URLs
+	invalidTests := []string{}
+	for _, test := range invalidTests {
+		_, err := extractDomain(test)
+		if err == nil {
+			t.Errorf("extractDomain(%q) did not return an error", test)
+		}
+	}
+}

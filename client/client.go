e7be4f506db88aab6069d69afa276ba778f437df
Pablo Ocampo <38480731+pablocampogo@users.noreply.github.com>
2023-05-02 16:10:10 -0400

    fix: delete connection close (#25)
    * fix: delete connection close mandatory header
    
    * mend

--- client/client.go
+++ client/client.go
@@ -70,7 +70,6 @@ func (c *Client) PostWithURLJSONParams(url string, params any, headers http.Head
 	}
 
 	headers.Set("Content-Type", "application/json")
-	headers.Set("Connection", "close")
 
 	req.Header = headers
 
@@ -91,7 +90,6 @@ func (c *Client) PostWithURLEncodedParams(url string, params url.Values, headers
 	}
 
 	headers.Set("Content-Type", "application/x-www-form-urlencoded")
-	headers.Set("Connection", "close")
 
 	req.Header = headers
 
@@ -111,7 +109,6 @@ func (c *Client) PutWithURLJSONParams(url string, params any, headers http.Heade
 	}
 
 	headers.Set("Content-Type", "application/json")
-	headers.Set("Connection", "close")
 
 	req.Header = headers
 

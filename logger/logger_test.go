fbe2a4eda9689a3367b4b43fb55c7beedc386424
commoddity <pascal@pokt.network>
2023-08-10 21:51:24 -0700

    feat: adds minimal logger implementation with unit tests

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -2,150 +2,119 @@ package logger
 
 import (
 	"bytes"
-	"errors"
+	"io"
+	"os"
 	"strings"
 	"testing"
+	"time"
 
-	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
+	"github.com/stretchr/testify/require"
 )
 
-type MockConsoleWriter struct {
-	captured bytes.Buffer
-}
-
-func (m *MockConsoleWriter) Write(p []byte) (n int, err error) {
-	m.captured.Write(p)
-	return len(p), nil
-}
-
-func (m *MockConsoleWriter) String() string {
-	return m.captured.String()
-}
-
-type testStruct struct {
-	StringValue   string
-	IntValue      int
-	BoolValue     bool
-	IgnoredField1 string
-	IgnoredField2 int
-}
-
-func (t testStruct) LogProperties() map[string]any {
-	return map[string]any{
-		"stringProp": t.StringValue,
-		"intProp":    t.IntValue,
-		"boolProp":   t.BoolValue,
-	}
-}
-
-func (t testStruct) LogName() string {
-	return "demoTest-struct"
-}
-
-func TestLogger(t *testing.T) {
-	mockConsoleWriter := &MockConsoleWriter{}
-	logger := &Logger{
-		ServiceName:    "test-service",
-		HasProbability: false,
-		Probability:    0.0,
-		Output: zap.New(
-			zapcore.NewCore(
-				zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig()),
-				zapcore.AddSync(mockConsoleWriter),
-				zapcore.DebugLevel,
-			)),
-	}
+func Test_Logger(t *testing.T) {
+	c := require.New(t)
 
 	tests := []struct {
-		name       string
-		eventName  string
-		objects    []LogObject
-		demoStruct testStruct
-		wantOutput []string
-		logMethod  func(logger *Logger, eventName string, objects []LogObject)
+		name        string
+		envLogLevel string
+		expectedOut string
 	}{
 		{
-			name:      "Info log with single object",
-			eventName: "testEvent",
-			demoStruct: testStruct{
-				StringValue: "infoTest",
-				IntValue:    78,
-			},
-			objects: []LogObject{
-				InfoObject("info error: something went wrong but it's ok tho"),
-			},
-			wantOutput: []string{
-				`testEvent`,
-				`"infoMsg": {"msg": "info error: something went wrong but it's ok tho"}`,
-				`"demoTest-struct"`,
-				`"stringProp": "infoTest"`,
-				`"intProp": 78`,
-				`"boolProp": false`,
-			},
-			logMethod: (*Logger).Info,
+			name:        "Should log at debug level",
+			envLogLevel: "debug",
+			expectedOut: "time=<CURRENT TIMESTAMP> level=DEBUG msg=\"Debug message\"\ntime=<CURRENT TIMESTAMP> level=INFO msg=\"Info message\"\ntime=<CURRENT TIMESTAMP> level=WARN msg=\"Warn message\"\ntime=<CURRENT TIMESTAMP> level=ERROR msg=\"Error message\"",
 		},
 		{
-			name:      "Warning log with multiple objects",
-			eventName: "warningEvent",
-			demoStruct: testStruct{
-				StringValue: "warnTest",
-				BoolValue:   true,
-			},
-			objects: []LogObject{
-				WarnObject(errors.New("Warning: app not found in this region")),
-			},
-			wantOutput: []string{
-				`warningEvent`,
-				`"warnMsg": {"error": "Warning: app not found in this region"}`,
-				`"demoTest-struct"`,
-				`"stringProp": "warnTest"`,
-				`"boolProp": true`,
-			},
-			logMethod: (*Logger).Warning,
+			name:        "Should log at info level",
+			envLogLevel: "info",
+			expectedOut: "time=<CURRENT TIMESTAMP> level=INFO msg=\"Info message\"\ntime=<CURRENT TIMESTAMP> level=WARN msg=\"Warn message\"\ntime=<CURRENT TIMESTAMP> level=ERROR msg=\"Error message\"",
 		},
 		{
-			name:      "Error log with multiple objects",
-			eventName: "errorEvent",
-			demoStruct: testStruct{
-				StringValue: "errTest",
-			},
-			objects: []LogObject{
-				ErrObject(errors.New("error occurred")),
-				MapObject("extraData", map[string]interface{}{
-					"key1": "value1",
-					"key2": 123,
-				}),
-				ErrObject(nil),
-			},
-			wantOutput: []string{
-				`errorEvent`,
-				`"errorMsg": {"error": "error occurred"}`,
-				`"extraData"`,
-				`"key1": "value1"`,
-				`"key2": 123`,
-				`"demoTest-struct"`,
-				`"stringProp": "errTest"`,
-				`"boolProp": false`,
-			},
-			logMethod: (*Logger).Error,
+			name:        "Should log at warn level",
+			envLogLevel: "warn",
+			expectedOut: "time=<CURRENT TIMESTAMP> level=WARN msg=\"Warn message\"\ntime=<CURRENT TIMESTAMP> level=ERROR msg=\"Error message\"",
+		},
+		{
+			name:        "Should log at error level",
+			envLogLevel: "error",
+			expectedOut: "time=<CURRENT TIMESTAMP> level=ERROR msg=\"Error message\"",
+		},
+		{
+			name:        "Should default to logging at info level if invalid log level is set",
+			envLogLevel: "what_is_this",
+			expectedOut: "time=<CURRENT TIMESTAMP> level=INFO msg=\"Info message\"\ntime=<CURRENT TIMESTAMP> level=WARN msg=\"Warn message\"\ntime=<CURRENT TIMESTAMP> level=ERROR msg=\"Error message\"",
+		},
+		{
+			name:        "Should default to logging at info level if no log level is set",
+			envLogLevel: "",
+			expectedOut: "time=<CURRENT TIMESTAMP> level=INFO msg=\"Info message\"\ntime=<CURRENT TIMESTAMP> level=WARN msg=\"Warn message\"\ntime=<CURRENT TIMESTAMP> level=ERROR msg=\"Error message\"",
 		},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			// Reset the captured console output for the next test case
-			mockConsoleWriter.captured.Reset()
-			tt.objects = append(tt.objects, tt.demoStruct)
-			tt.logMethod(logger, tt.eventName, tt.objects)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// Set environment variable for the test
+			err := os.Setenv(logLevel, test.envLogLevel)
+			c.NoError(err)
 
-			// Read and assert the captured console output
-			capturedOutput := mockConsoleWriter.String()
+			// Create a pipe to capture standard error output
+			r, w, err := os.Pipe()
+			c.NoError(err)
+			originalStderr := os.Stderr
+			os.Stderr = w
 
-			for _, want := range tt.wantOutput {
-				if !strings.Contains(capturedOutput, want) {
-					t.Errorf("Expected console output to contain '%s', got: '%s'", want, capturedOutput)
+			// Create the logger using the New function
+			logger := New()
+
+			// Log the messages at different levels
+			logger.Debug("Debug message")
+			logger.Info("Info message")
+			logger.Warn("Warn message")
+			logger.Error("Error message")
+
+			// Restore original stderr
+			os.Stderr = originalStderr
+			w.Close()
+
+			now := time.Now()
+
+			// Replace the placeholder for the current timestamp
+			expectedOut := strings.ReplaceAll(test.expectedOut, "<CURRENT TIMESTAMP>", now.Format("2006-01-02T15:04:05.000-07:00"))
+
+			// Read the captured output
+			var buffer bytes.Buffer
+			_, err = io.Copy(&buffer, r)
+			c.NoError(err)
+
+			actualOutput := buffer.String()
+			actualLines := strings.Split(strings.TrimSpace(actualOutput), "\n")
+			expectedLines := strings.Split(strings.TrimSpace(expectedOut), "\n")
+
+			// Compare the number of lines
+			c.Equal(len(expectedLines), len(actualLines))
+
+			// Compare the timestamps and the rest of each line
+			for i := 0; i < len(expectedLines); i++ {
+				actualTimestamp := strings.SplitN(actualLines[i], " ", 2)[0][5:]
+
+				// Try parsing the timestamp in the first format
+				parsedTime, err := time.Parse("2006-01-02T15:04:05.000-07:00", actualTimestamp)
+				if err != nil {
+					// If the first format fails, try the second format
+					parsedTime, err = time.Parse(time.RFC3339, actualTimestamp)
 				}
+				c.NoError(err, "Timestamp is not in the expected format")
+
+				c.True(now.Sub(parsedTime) < 100*time.Millisecond, "Timestamp is not within 100ms of current time")
+
+				// Convert the expected line to use "Z" if the timezone is UTC
+				expectedLine := expectedLines[i]
+				if parsedTime.UTC().Equal(parsedTime) {
+					expectedLine = strings.ReplaceAll(expectedLine, "+00:00", "Z")
+				}
+
+				// Compare the rest of the line
+				c.Equal(expectedLine, actualLines[i])
 			}
 		})
 	}
